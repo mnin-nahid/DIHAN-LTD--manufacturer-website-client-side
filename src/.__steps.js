28acfa05fc46bcc558005c81cf4bf0935e34afbc
mnin-nahid <nahidulislam.nayan54@gmail.com>
2022-05-22 03:01:01 +0600

    project setup

---
+++ src/.__steps.js
@@ -0,0 +1,11 @@
+/**
+ * -------------------
+ *      22-05-2022
+ * -------------------
+ * 1. create react app with company name.
+ * 2. clean code. 
+ * 3. install tailwind css.
+ * 4. install react router.
+ * 5. install daisyUI.
+ * 6. First code push to github. 
+ */
--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-4xl text-primary font-bold text-center'>Hello Form Dihan</h1>
+      <Routes>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
