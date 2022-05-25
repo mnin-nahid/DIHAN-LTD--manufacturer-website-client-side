f4f44819f705cce2a3f5d8b8d33e8ed921b9c095
mnin-nahid <nahidulislam.nayan54@gmail.com>
2022-05-25 07:07:36 +0600

    structure review

--- src/pages/Home/Testimonial.js
+++ src/pages/Home/Testimonial.js
@@ -2,34 +2,60 @@ import React from 'react';
 
 const Testimonial = () => {
     return (
-        <div>
+        <div className='my-10'>
             <h2 className='text-3xl font-bold text-center'>Testimonial</h2>
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:px-14'>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quia deserunt itaque eos non nihil explicabo laborum officiis quae exercitationem veritatis, ducimus, veniam ut nemo dolore excepturi repellendus voluptatem beatae.</p>
+                        <div className='flex space-x-3 justify-center items-center'>
+                            <div tabindex="0" class="btn btn-ghost btn-circle avatar">
+                                <div class="w-16 rounded-full">
+                                    <img src="https://api.lorem.space/image/face?hash=33791" alt='' />
+                                </div>
+                            </div>
+                            <div>
+                                <h2 className='text-xl font-semibold'>Nahidul Islam</h2>
+                                <p>* * * * *</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quia deserunt itaque eos non nihil explicabo laborum officiis quae exercitationem veritatis, ducimus, veniam ut nemo dolore excepturi repellendus voluptatem beatae.</p>
+                        <div className='flex space-x-3 justify-center items-center'>
+                            <div tabindex="0" class="btn btn-ghost btn-circle avatar">
+                                <div class="w-16 rounded-full">
+                                    <img src="https://api.lorem.space/image/face?hash=33791" alt='' />
+                                </div>
+                            </div>
+                            <div>
+                                <h2 className='text-xl font-semibold'>Nahidul Islam</h2>
+                                <p>* * * * *</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quia deserunt itaque eos non nihil explicabo laborum officiis quae exercitationem veritatis, ducimus, veniam ut nemo dolore excepturi repellendus voluptatem beatae.</p>
+                        <div className='flex space-x-3 justify-center items-center'>
+                            <div tabindex="0" class="btn btn-ghost btn-circle avatar">
+                                <div class="w-16 rounded-full">
+                                    <img src="https://api.lorem.space/image/face?hash=33791" alt='' />
+                                </div>
+                            </div>
+                            <div>
+                                <h2 className='text-xl font-semibold'>Nahidul Islam</h2>
+                                <p>* * * * *</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                
             </div>
         </div>
     );
